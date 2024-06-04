1e6a36fdf3fdf03152920c1dca1afd406b3ad58e
Augusto de Nevrezé <adenevreze@gmail.com>
2024-06-04 07:57:56 -0300

    Added poetry to execute things in local. Added kafka_producer to send packets to kafka topic

---
+++ pyflink/usr_jobs/kafka_producer.py
@@ -0,0 +1,50 @@
+import json
+import random
+import time
+from datetime import datetime, timezone
+from typing import Any
+import uuid
+
+from kafka import KafkaProducer
+
+SLEEP_TIME = 0.5
+
+def generate_sensor_data() -> dict[str, Any]:
+    """Generates random sensor data. It also adds a timestamp for traceability."""
+    sensor_id = f"sensor_{random.randint(1, 5)}"
+    temperature = round(random.uniform(15, 35), 2)
+    pressure = round(random.uniform(950, 1050), 2)
+    vibration = round(random.uniform(0, 10), 2)
+    sensor_data = {
+        "message_id": uuid.uuid4().hex,
+        "sensor_id": sensor_id,
+        "data": {
+            "temperature": temperature,
+            "pressure": pressure,
+            "vibration": vibration,
+        },
+        # utc timestamp
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+    }
+    return sensor_data
+
+
+def main() -> None:
+    """
+    Controls the flow of the producer. It first subscribes to the topic and then
+    generates sensor data and sends it to the topic.
+    """
+    producer = KafkaProducer(
+        bootstrap_servers="localhost:9092",
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    )
+
+    while True:
+        sensor_data = generate_sensor_data()
+        producer.send("sensors", value=sensor_data)
+        print(f"Produced: {sensor_data}")
+        time.sleep(SLEEP_TIME)
+
+
+if __name__ == "__main__":
+    main()
