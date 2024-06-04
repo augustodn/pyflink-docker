677ba82b9efce54833e883521295f005ccb9474a
Augusto de Nevrezé <adenevreze@gmail.com>
2024-06-04 17:52:46 -0300

    Added kafka source

--- pyflink/usr_jobs/postgres_sink.py
+++ pyflink/usr_jobs/postgres_sink.py
@@ -1,23 +1,37 @@
+import json
+
+from pyflink.common import Types, WatermarkStrategy
+from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.common import Types
 from pyflink.datastream.connectors import JdbcSink
-from pyflink.datastream.connectors.jdbc import JdbcConnectionOptions, JdbcExecutionOptions
+from pyflink.datastream.connectors.jdbc import (
+    JdbcConnectionOptions,
+    JdbcExecutionOptions,
+)
+from pyflink.datastream.connectors.kafka import KafkaOffsetsInitializer, KafkaSource
+from pyflink.common import Row
+from datetime import datetime
+
+def parse_data(data: str) -> Row:
+    data = json.loads(data)
+    message_id = data["message_id"]
+    sensor_id = int(data["sensor_id"])
+    message = json.dumps(data["message"])
+    timestamp = datetime.strptime(data["timestamp"], "%Y-%m-%dT%H:%M:%S.%f+00:00")
+    return Row(message_id, sensor_id, message, timestamp)
+
 
 # Define the Stream Execution Environment
 env = StreamExecutionEnvironment.get_execution_environment()
 
 # Define the data type
-type_info = Types.ROW([Types.INT(), Types.STRING(), Types.STRING(), Types.INT()])
-
-# Create a DataStream from a collection
-data_stream = env.from_collection(
+type_info = Types.ROW(
     [
-        (101, "Stream Processing with Apache Flink", "Fabian Hueske, Vasiliki Kalavri", 2019),
-        (102, "Streaming Systems", "Tyler Akidau, Slava Chernyak, Reuven Lax", 2018),
-        (103, "Designing Data-Intensive Applications", "Martin Kleppmann", 2017),
-        (104, "Kafka: The Definitive Guide", "Gwen Shapira, Neha Narkhede, Todd Palino", 2017)
-    ],
-    type_info=type_info
+        Types.STRING(),  # message_id
+        Types.INT(),  # sensor_id
+        Types.STRING(),  # message
+        Types.SQL_TIMESTAMP(),  # timestamp
+    ]
 )
 
 
@@ -28,30 +42,65 @@ current_dir = "/".join(current_dir_list)
 # Adding the jar to the flink streaming environment
 env.add_jars(
     f"file://{current_dir}/flink-connector-jdbc-3.1.2-1.18.jar",
-    f"file://{current_dir}/postgresql-42.7.3.jar"
+    f"file://{current_dir}/postgresql-42.7.3.jar",
+    f"file://{current_dir}/flink-sql-connector-kafka-3.1.0-1.18.jar",
 )
 
+properties = {
+    "bootstrap.servers": "localhost:9092",
+    "group.id": "iot-sensors",
+}
 
+earliest = False
+offset = (
+    KafkaOffsetsInitializer.earliest() if earliest else KafkaOffsetsInitializer.latest()
+)
+
+# Create a Kafka Source
+# NOTE: FlinkKafkaConsumer class is deprecated
+kafka_source = (
+    KafkaSource.builder()
+    .set_topics("sensors")
+    .set_properties(properties)
+    .set_starting_offsets(offset)
+    .set_value_only_deserializer(SimpleStringSchema())
+    .build()
+)
+
+# Create a DataStream from the Kafka source and assign watermarks
+data_stream = env.from_source(
+    kafka_source, WatermarkStrategy.no_watermarks(), "Kafka sensors topic"
+)
+
+# Print line for readablity in the console
+print("start reading data from kafka")
+
+
+transformed_data = data_stream.map(parse_data, output_type=type_info)
 
 # Define the JDBC Sink
 jdbc_sink = JdbcSink.sink(
-    "INSERT INTO books (id, title, authors, year) VALUES (?, ?, ?, ?)",
-	type_info,
+    "INSERT INTO raw_sensors_data (message_id, sensor_id, message, timestamp) VALUES (?, ?, ?, ?)",
+    type_info,
     JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
-        .with_url('jdbc:postgresql://localhost:5432/flinkdb')
-        .with_driver_name('org.postgresql.Driver')
-        .with_user_name('flinkuser')
-        .with_password('flinkpassword')
-        .build(),
+    .with_url("jdbc:postgresql://localhost:5432/flinkdb")
+    .with_driver_name("org.postgresql.Driver")
+    .with_user_name("flinkuser")
+    .with_password("flinkpassword")
+    .build(),
     JdbcExecutionOptions.builder()
-        .with_batch_interval_ms(1000)
-        .with_batch_size(200)
-        .with_max_retries(5)
-        .build()
+    .with_batch_interval_ms(1000)
+    .with_batch_size(200)
+    .with_max_retries(5)
+    .build(),
 )
 
+
 # Add the JDBC sink to the data stream
-data_stream.add_sink(jdbc_sink)
+transformed_data.print()
+transformed_data.add_sink(jdbc_sink)
+print("start writing data to postgresql")
+
 
 # Execute the Flink job
 env.execute("Flink PostgreSQL Sink Example")
